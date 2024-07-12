0665723fb567d7806e4976cf4ab00a9f3b7680a8
NicolasRaynaud1 <nraynaud.pro@gmail.com>
2024-07-12 17:11:07 +0200

    modification du middleware d'auth pour récupérer les données de l'utilsiateur

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,15 +1,16 @@
 const UnauthorizedError = require("../errors/unauthorized");
 const jwt = require("jsonwebtoken");
 const config = require("../config");
+const userService = require('./../api/users/users.service');
 
-module.exports = (req, res, next) => {
+module.exports = async (req, res, next) => {
   try {
     const token = req.headers["x-access-token"];
     if (!token) {
       throw "not token";
     }
-    const decoded = jwt.verify(token, config.secretJwtToken);
-    req.user = decoded;
+    const jwtInfos = jwt.verify(token, config.secretJwtToken);
+    req.user = await userService.get(jwtInfos.userId);
     next();
   } catch (message) {
     next(new UnauthorizedError(message));
