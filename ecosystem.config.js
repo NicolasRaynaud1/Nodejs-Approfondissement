69db61740c9ab6a0da309759937dc4a5d82f780a
NicolasRaynaud1 <nraynaud.pro@gmail.com>
2024-07-14 16:56:01 +0200

    ajout de la configuration de déploiement

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,6 +6,10 @@ module.exports = {
       env_production: {
         NODE_ENV: "production",
       },
+      error_file: "./logs/err.log",
+      max_memory_restart: "200M",
+      instances: 3,
+      exec_mode: "cluster"
     },
   ],
 };
